6534020792c97dc96492f84b975c8c4e3bdeefbb
Xburyl00 <127678069+Xburyl00@users.noreply.github.com>
2024-04-05 02:50:24 +0300

    Update ToolEnvironment.java

--- ija/ija2023/homework2/tool/common/ToolEnvironment.java
+++ ija/ija2023/homework2/tool/common/ToolEnvironment.java
@@ -6,11 +6,14 @@
 // ToolEnvironment.java
 package ija.ija2023.homework2.tool.common;
 
+import ija.ija2023.homework2.common.Obstacle;
+
 import java.util.List;
 
 public interface ToolEnvironment {
     boolean obstacleAt(Position p);
     int rows();
     int cols();
-    List<ToolRobot> robots(); // Fix import or definition of ToolRobot
+    List<ToolRobot> robots();
+    List<Obstacle> myObstacleslist();
 }
